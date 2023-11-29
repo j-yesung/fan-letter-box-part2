4651c542dfab2045e59cfdf6a111e9633462d67b
j-yesung <hisung-ah@kakao.com>
2023-11-29 18:52:45 +0900

    로그인, 회원가입 라우터 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,7 @@
 import Layout from 'layout/Layout';
-import Main from 'pages/Main';
+import Main from 'pages/Home/Main';
+import Login from 'pages/User/Login';
+import SignUp from 'pages/User/SignUp';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -7,6 +9,8 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Main />} />
         </Route>
