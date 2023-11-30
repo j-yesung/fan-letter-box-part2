5b5b800ec4a8c63bb4989f5db299ee0b0939b9c0
j-yesung <hisung-ah@kakao.com>
2023-11-30 14:35:52 +0900

    로그인, 회원가입 페이지 생성

--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import * as S from './User.styled.js';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  /**
+   * TODO : JWT token 로그인 진행
+   */
+  const handleLogin = () => {};
+
   return (
     <>
-      <div>Login</div>
+      <S.USER_CONTAINER>
+        <S.USER_TITLE>로그인</S.USER_TITLE>
+        <form>
+          <div>
+            <S.INPUT type="text" placeholder="아이디" required />
+          </div>
+          <div>
+            <S.INPUT type="password" placeholder="비밀번호" required />
+          </div>
+          <div>
+            <S.USER_BUTTON onClick={handleLogin}>로그인</S.USER_BUTTON>
+          </div>
+        </form>
+        <S.CAPTION onClick={() => navigate('/signup')}>회원가입</S.CAPTION>
+      </S.USER_CONTAINER>
     </>
   );
 };
--- src/pages/User/SignUp.jsx
+++ src/pages/User/SignUp.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import * as S from './User.styled';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>SignUp</div>
+      <S.USER_CONTAINER>
+        <S.USER_TITLE>회원가입</S.USER_TITLE>
+        <form>
+          <div>
+            <S.INPUT type="text" placeholder="아이디" required />
+          </div>
+          <div>
+            <S.INPUT type="password" placeholder="비밀번호" required />
+          </div>
+          <div>
+            <S.INPUT type="text" placeholder="닉네임" required />
+          </div>
+          <div>
+            <S.USER_BUTTON>회원가입</S.USER_BUTTON>
+          </div>
+        </form>
+        <S.CAPTION onClick={() => navigate('/login')}>로그인</S.CAPTION>
+      </S.USER_CONTAINER>
     </>
   );
 };
