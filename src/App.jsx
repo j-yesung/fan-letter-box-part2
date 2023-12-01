de7c39652321e6644e6a113ad5cd94ec89f70e21
j-yesung <hisung-ah@kakao.com>
2023-12-01 10:12:47 +0900

    -

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Router from 'shared/Router';
 import GlobalStyles from './styles/GlobalStyles';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setAccessToken } from 'redux/modules/userInfoSlice';
+import { setAccessToken } from 'redux/modules/authSlice';
 
 function App() {
   const dispatch = useDispatch();
