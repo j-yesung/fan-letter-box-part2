2e3b2851ee6c6229339c69f0a49a3d0c7347695b
j-yesung <hisung-ah@kakao.com>
2023-12-02 01:52:16 +0900

    유저 정보 슬라이스 수정

--- src/redux/modules/authSlice.js
+++ src/redux/modules/authSlice.js
@@ -1,48 +1,18 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import API from '../../apis/api/user';
-
-export const fetchUserInfo = createAsyncThunk('userInfo/fetchUserInfo', async (accessToken, thunkAPI) => {
-  try {
-    const response = await API.get('/user', {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue('공습 경보!' + error.message);
-  }
-});
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  accessToken: null,
   userInfo: null,
-  loading: false,
-  error: null,
 };
 
 export const userInfoSlice = createSlice({
   name: 'userInfo',
   initialState,
   reducers: {
-    setAccessToken: (state, action) => {
-      state.accessToken = action.payload;
+    setUserInfo: (state, action) => {
+      state.userInfo = action.payload;
     },
-  },
-  extraReducers: builder => {
-    builder
-      .addCase(fetchUserInfo.pending, state => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchUserInfo.fulfilled, (state, action) => {
-        state.loading = false;
-        state.userInfo = action.payload;
-      })
-      .addCase(fetchUserInfo.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      });
   },
 });
 
-export const { setAccessToken, setUserInfo } = userInfoSlice.actions;
+export const { setUserInfo } = userInfoSlice.actions;
 export default userInfoSlice.reducer;
