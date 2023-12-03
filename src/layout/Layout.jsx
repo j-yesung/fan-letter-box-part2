b07a1b0106016c1457cdc38ae4bedaba39173921
j-yesung <hisung-ah@kakao.com>
2023-12-03 21:14:38 +0900

    home, profile URL 직접 접근 막기 (유저 정보로 분기처리)

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 const Layout = () => {
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+  const isLoggedIn = !!userInfo; // 로그인 여부 확인
   return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
+      {isLoggedIn ? (
+        <>
+          <Header />
+          <Outlet />
+          <Footer />
+        </>
+      ) : (
+        <Navigate to="/" replace />
+      )}
     </>
   );
 };
